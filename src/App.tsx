cb8dcbbe69626db949909c90b6ee9f527edb90c3
Zahra <bghzahra@outlook.com>
2022-03-21 11:48:48 +0000

    imported singleEpisode file - zahra

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,9 @@
-import episodes from "./tvData.json";
-console.log(`Imported ${episodes.length} episode(s)`);
-console.log(`First episode's name is ${episodes[0].name}`);
+import SingleEpisode from "./components/singleEpisode";
 
-const episodesArr:IntEpisode[] = episodes
-
-interface IntEpisode {
-  id: number;
-  url: string;
-  name: string;
-  season: number;
-  number: number;
-  type: string;
-  airdate: string;
-  airtime: string;
-  airstamp: string;
-  runtime: number;
-  image: {
-    medium: string;
-    original: string;
-  };
-  summary: string;
-  _links: { self: { href: string } };
-}
-
-const mapId = episodesArr.map((value)=> <li key={value.id}> {value.name} </li>)
 function App(): JSX.Element {
   return (
     <>
-      <li>{mapId}</li>
+      <SingleEpisode/>
     </>
   );
 
