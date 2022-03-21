8d007deeac71912db221fa499dd2853de9d88167
Roshni Patel <roshnihpatel@outlook.com>
2022-03-21 10:36:39 +0000

    json file not correctly imported

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import { greet } from "./utils/greet";
+import episodes from "./tvData.json";
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  return (
+    <>
+      <h1>{greet("World")}</h1>;{console.log(`Imported ${episodes} episode(s)`)}
+    </>
+  );
 }
 
 export default App;
