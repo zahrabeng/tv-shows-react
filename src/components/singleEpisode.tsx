ab82e62db3de5ac7cf634f0b3440cd611e105c69
Roshni Patel <roshnihpatel@outlook.com>
2022-03-21 12:23:23 +0000

    formatted and typed checked -roshni

--- src/components/singleEpisode.tsx
+++ src/components/singleEpisode.tsx
@@ -8,25 +8,26 @@ IMAGE OF EPISODE
 episode summary - P TAG
 */
 
-
 import IntEpisode from "./episodesTypes";
-import episodeList from "../tvData.json"
+import episodeList from "../tvData.json";
 
+const episodesArr: IntEpisode[] = episodeList;
+const mapName = episodesArr.map((value) => (
+  <div key={value.id}>
+    {" "}
+    <h2>
+      {" "}
+      {value.name} {value.season} {value.number}
+    </h2>
+    <img src={value.image.medium} alt="screenshot from episode" />
+    {value.summary.replace(/<\/?p>/g, " ")}
+  </div>
+));
 
-const episodesArr:IntEpisode[] = episodeList
-const mapName = episodesArr.map((value) => 
-<div key={value.id}> <h2> {value.name} {value.season} {value.number}</h2>
-<img src={value.image.medium} alt="screenshot from episode"/>
-{value.summary.replace(/<\/?p>/g ," ")}
-</div>
-)
-
-export default function SingleEpisode():JSX.Element{
-    return(
-        <>
-            <div>{mapName}</div>
-        </>
-    )
+export default function SingleEpisode(): JSX.Element {
+  return (
+    <>
+      <div>{mapName}</div>
+    </>
+  );
 }
-
-
