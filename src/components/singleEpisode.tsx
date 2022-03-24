af77866510589cf07b555583eb5bdda3e5840d8a
Zahra <bghzahra@outlook.com>
2022-03-24 16:01:01 +0000

    fixed getch bug and managed to render all episodes with async and useEffect - zahra

--- src/components/singleEpisode.tsx
+++ src/components/singleEpisode.tsx
@@ -1,50 +1,30 @@
 import IntEpisode from "./episodesTypes";
-//import episodeList from "../simpsonsData.json";
 import seasonEpisodeNum from "./seasonEpisodeNum";
 import MatchToSearch from "./matchToSearch";
 import SummaryCleaning from "./summaryCleaning";
 import { useEffect, useState } from "react";
-import { Z_STREAM_ERROR } from "zlib";
 
 export default function SingleEpisode(): JSX.Element {
   const [searchText, setSearchText] = useState<string>("");
   const [episode, setEpisode] = useState<IntEpisode[]>([]);
-  
-  let allEpisodes: IntEpisode[] = []
+
+  useEffect(() => {
     const handleGetEpisodes = async () => {
-      const response = await fetch(
-        "https://api.tvmaze.com/shows/82/episodes "
-      )
-      const episodeList:IntEpisode[] = await response.json()
-      
-      allEpisodes = [...episodeList]
-      
-      //setEpisode([ episodeList])
-    }
-    
-    // const handleGetEpisodes = () =>{
-    //   fetch("https://api.tvmaze.com/shows/82/episodes")
-    //   .then((response) => response.json())
-    //   .then((episodeList:IntEpisode) => allEpisodes =  episodeList)
-      
-    // }
-      
-    useEffect(()=> {
-      handleGetEpisodes()
-      console.log(allEpisodes)
-      console.log("render")
-      
-    },[])
+      const response = await fetch("https://api.tvmaze.com/shows/82/episodes ");
+      const episodeList: IntEpisode[] = await response.json();
+      setEpisode([...episodeList]);
+    };
+    handleGetEpisodes();
+  }, [episode]);
 
-
-  // allEpisodes  = [...episode];
-
-  const filterEpisodes = allEpisodes.filter((oneEpisode: IntEpisode) => {
-
-      return (MatchToSearch(oneEpisode.name, searchText) || MatchToSearch(oneEpisode.summary , searchText))
+  const filterEpisodes = episode.filter((oneEpisode: IntEpisode) => {
+    return (
+      MatchToSearch(oneEpisode.name, searchText) ||
+      MatchToSearch(oneEpisode.summary, searchText)
+    );
   });
 
-  const displayEpisodes = `Displaying ${filterEpisodes.length}/${allEpisodes.length} episodes.`;
+  const displayEpisodes = `Displaying ${filterEpisodes.length}/${episode.length} episodes.`;
 
   const mapName = filterEpisodes.map((value) => (
     <div key={value.id}>
@@ -53,7 +33,9 @@ export default function SingleEpisode(): JSX.Element {
         {" "}
         {value.name} {seasonEpisodeNum(value.season, value.number)}
       </h2>
-      {(value.image) && <img src={value.image.medium} alt="screenshot from episode"/>}
+      {value.image && (
+        <img src={value.image.medium} alt="screenshot from episode" />
+      )}
       {SummaryCleaning(value.summary)}
     </div>
   ));
