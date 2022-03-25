63731f6f0e64086053b85829a41184c18e6c0bd6
Roshni Patel <roshnihpatel@outlook.com>
2022-03-25 11:17:32 +0000

    added all episode types as an interface

---
+++ src/components/showTypes.tsx
@@ -0,0 +1,68 @@
+export default interface showTypes{
+    id: number
+    url:string
+    name: string
+    type: string
+    language: string
+    genres: string[]
+    status: string
+    runtime: number
+    averageRuntime: number
+    premiered: string
+    ended: string
+    officialSite: string
+    schedule: {
+        time: string
+        days: string[]
+    }
+    rating: {
+        average: null | string
+    }
+    weight: number
+    network: {
+        id: number
+        name:string
+        country: {
+            name:string
+            code:string
+            timezone: string
+
+        }
+        officialSite: null | string
+
+    }
+    webChannel: {
+        id: number
+        name:string
+        country: {
+            name:string
+            code:string
+            timezone: string
+
+        }
+        officialSite: null | string
+
+    }
+    dvdCountry: null |string
+    externals: {
+        tvrage:number
+        thetvdb: number
+        imbd: string
+
+    }
+    image: {
+        medium: string
+        original:string
+
+    }
+    summary: string
+    updated: number
+    _links: { self: { href: string } };
+    previousepisode: {
+        href: string
+
+    }
+
+ 
+   
+}
